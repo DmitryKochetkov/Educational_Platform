5d3d2ee3e57fffdaf05a233f8514f3004370cb78
Dmitry Kochetkov <dimitri.kochetkov@gmail.com>
2020-01-23 17:12:05 +0300

    Admin: fix mappings in html

--- src/main/java/mirea/dimedrol228/edu/Controllers/AdminController.java
+++ src/main/java/mirea/dimedrol228/edu/Controllers/AdminController.java
@@ -3,6 +3,7 @@ package mirea.dimedrol228.edu.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/admin")
@@ -10,5 +11,16 @@ public class AdminController {
     @GetMapping
     public String admin() {
         return "admin";
+    }
+
+    @GetMapping("/manage-hashtags")
+    public String manage_hashtags() {
+        return "manage-hashtags";
+    }
+
+    @GetMapping("/manage-hashtags/edit")
+    public String manage_hashtags_edit(@RequestParam String hashtag_name, @RequestParam String hashtag_description) {
+        //TODO: hashtag logic
+        return "redirect:"; //??
     }
 }
